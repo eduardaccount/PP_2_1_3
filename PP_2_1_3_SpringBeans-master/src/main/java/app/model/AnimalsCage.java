1a5ed47dfff3a53d9ecbe5cc76774fbb8c51c5b2
eduardaccount <100372621+eduardaccount@users.noreply.github.com>
2022-03-08 20:40:35 +0500

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,33 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+    private Timer timer;
+
+    public AnimalsCage() {
+        this.timer = new Timer();
+    }
+
+    @Bean
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
